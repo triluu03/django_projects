a0180cc4256a989e723733159545482418da7d64
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2023-10-31 14:48:40 +0200

    Create the structure for the project

---
+++ tic_tac_toe/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Tic Tac Toe game is being developed!")
