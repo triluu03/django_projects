a027c196a4d9f0c92cb87db72e6dc7bf61dd5bdc
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2023-11-28 08:55:29 +0200

    heroku deployment fixes

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 release: python manage.py migrate
-web: daphne django_projects.asgi:application --port $PORT --bind 0.0.0.0
-worker: python manage.py runworker channel_layer
+web: daphne django_projects.asgi:application --port $PORT --bind 0.0.0.0 -v2
+worker: python manage.py runworker channels --settings=django_projects.settings -v2
--- django_projects/asgi.py
+++ django_projects/asgi.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_projects.settings")
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -16,9 +20,6 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 import tic_tac_toe.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_projects.settings")
-django.setup()
 
 django_asgi_app = get_asgi_application()
 
