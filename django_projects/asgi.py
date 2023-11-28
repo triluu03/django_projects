236968e437dac92732c4ffb303a55f25171c0f0f
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2023-11-28 08:47:28 +0200

    heroku setup

--- django_projects/asgi.py
+++ django_projects/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -17,6 +18,7 @@ from django.core.asgi import get_asgi_application
 import tic_tac_toe.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_projects.settings")
+django.setup()
 
 django_asgi_app = get_asgi_application()
 
